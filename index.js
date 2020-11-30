c24a5526f86c0568c038f6365dd97be8ae11d189
codingpath102 <69256780+codingpath102@users.noreply.github.com>
2020-11-30 21:05:04 +0600

    upload completed

---
+++ index.js
@@ -0,0 +1,22 @@
+const name = document.getElementById('name');
+const password = document.getElementById('password');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('err');
+
+form.addEventListener('submit', (e) => {
+    let messages = [];
+    if (name.value === '' || name.value === null) {
+        messages.push('Name is required');
+    }
+
+    
+    
+    
+    if (messages.length > 0) {
+        e.preventDefault();
+        errorElement.innerText = messages.join(', ');
+    }
+    if (password.value.length <= 6) {
+        messages.push('Password should be longer');
+    }
+});
